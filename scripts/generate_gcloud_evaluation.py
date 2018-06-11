a0581cc3c49304358052175657c95625fa060b84
pomonam <pomonam15@gmail.com>
2018-06-11 10:40:48 -0400

    Added scripts to generate commands for gcloud

--- scripts/generate_gcloud_evaluation.py
+++ scripts/generate_gcloud_evaluation.py
@@ -28,36 +28,23 @@ MODEL_VERSION = 1
 FRAME_LEVEL = True
 # What features? e.g. RGB, audio
 FEATURES = "rgb,audio"
-# Batch size.
-BATCH_SIZE = 128
-# Base LR.
-BASE_LEARNING_RATE = 0.0002
-# Initialize a new model?
-START_NEW_MODEL = True
 
 
 def main():
     # Start by defining a job name.
-    command = "JOB_NAME=yt8m_train_$(date +%Y%m%d_%H%M%S); "
+    command = "JOB_NAME=yt8m_eval_$(date +%Y%m%d_%H%M%S); "
     command += "gcloud --verbosity=debug ml-engine jobs submit training $JOB_NAME "
     command += "--package-path=youtube-8m --module-name=youtube-8m.train"
     command += "--staging-bucket=$BUCKET_NAME --region=us-east1 "
     command += "--config=youtube-8m/{} ".format(CLOUD_GPU)
     if FRAME_LEVEL:
-        command += "-- --train_data_pattern='gs://youtube8m-ml-us-east1/2/frame/train/train*.tfrecord' "
+        command += "-- --eval_data_pattern='gs://youtube8m-ml-us-east1/2/frame/validate/validate*.tfrecord' "
         command += "--frame_features=True "
     else:
-        command += "-- --train_data_pattern='gs://youtube8m-ml-us-east1/2/video/train/train*.tfrecord' "
+        command += "-- --train_data_pattern='gs://youtube8m-ml-us-east1/2/video/validate/validate*.tfrecord "
         command += "--frame_features=False "
-    command += "--model={} ".format(MODEL_NAME)
-    command += "--feature_names='{}' ".format(FEATURES)
-    command += "--feature_sizes='1024,128' "
-    command += "--batch_size={} ".format(str(BATCH_SIZE))
     command += "--train_dir=$BUCKET_NAME/{} ".format(MODEL_NAME + str(MODEL_VERSION))
-    command += "--base_learning_rate={} ".format(str(BASE_LEARNING_RATE))
-    if START_NEW_MODEL:
-        command += "--start_new_model"
-    return command
+    command += "--run_once=True"
 
 
 if __name__ == "__main__":
