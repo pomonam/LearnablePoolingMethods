a15e2caf07a9675f026199ba2c2bb85ab2745e75
Ruijian <anruijian_sz@163.com>
2018-06-18 12:18:05 -0400

    fix cmd

--- scripts/generate_gcloud_train.py
+++ scripts/generate_gcloud_train.py
@@ -40,7 +40,7 @@ def main():
     # Start by defining a job name.
     command = "JOB_NAME=yt8m_train_$(date +%Y%m%d_%H%M%S); "
     command += "gcloud --verbosity=debug ml-engine jobs submit training $JOB_NAME "
-    command += "--package-path=youtube-8m --module-name=youtube-8m.train"
+    command += "--package-path=youtube-8m --module-name=youtube-8m.train "
     command += "--staging-bucket=$BUCKET_NAME --region=us-east1 "
     command += "--config=youtube-8m/cloudml_config/{} ".format(CLOUD_GPU)
     if FRAME_LEVEL:
