6e13dfcc444ad9dee8aa1ec4cc36f2bafd58e443
pomonam <pomonam15@gmail.com>
2018-06-19 11:46:47 -0400

    PN, NP Gate added

--- attention_modules.py
+++ attention_modules.py
@@ -33,19 +33,24 @@ flags.DEFINE_integer("CGV3_cluster_size", 7200,
 # Vocabulary Correlation ######################################################
 ###############################################################################
 class PnGateModule(modules.BaseModule):
-    def __init__(self, vocab_size, is_training, batch_norm=True, scope_id=None):
-        """
-        :param vocab_size:
-        :param is_training:
-        :param batch_norm:
-        :param scope_id:
+    def __init__(self, vocab_size, is_training, scope_id=None):
+        """ Initialize class PnGateModule.
+        :param vocab_size: int
+            Size of the classes.
+        :param is_training: bool
+            True iff the model is being trained.
+        :param scope_id: Object
         """
         self.vocab_size = vocab_size
-        self.batch_norm = batch_norm
         self.scope_id = scope_id
         self.is_training = is_training
 
     def forward(self, inputs, **unused_params):
+        """ PN Gate for correlation learning.
+        vocabularies -> P gate -> N gate -> output
+        :param inputs: batch_size x vocab_size
+        :return: batch_size x vocab_size
+        """
         p_gating_weights = \
             tf.get_variable("p_pn_gate",
                             [self.vocab_size, self.vocab_size],
@@ -61,12 +66,133 @@ class PnGateModule(modules.BaseModule):
         p_gate = inputs + p_activation
 
         # batch_size x vocab_size, vocab_size x vocab_size --> batch_size x vocab_size
-        n_activation = tf.matmul(inputs, n_gating_weights)
+        n_activation = tf.matmul(p_gate, n_gating_weights)
         n_activation = -1 * n_activation
         n_activation = tf.nn.relu6(n_activation)
         n_gate = p_gate + (-1 * n_activation)
 
         output = tf.nn.softmax(n_gate)
+        return output
+
+
+class NpGateModule(modules.BaseModule):
+    def __init__(self, vocab_size, is_training, scope_id=None):
+        """ Initialize class NpGateModule.
+        :param vocab_size: int
+            Size of the classes.
+        :param is_training: bool
+            True iff the model is being trained.
+        :param scope_id: Object
+        """
+        self.vocab_size = vocab_size
+        self.scope_id = scope_id
+        self.is_training = is_training
+
+    def forward(self, inputs, **unused_params):
+        """ PN Gate for correlation learning.
+        vocabularies -> N gate -> P gate -> output
+        :param inputs: batch_size x vocab_size
+        :return: batch_size x vocab_size
+        """
+        p_gating_weights = \
+            tf.get_variable("p_np_gate",
+                            [self.vocab_size, self.vocab_size],
+                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(self.vocab_size)))
+        n_gating_weights = \
+            tf.get_variable("n_np_gate",
+                            [self.vocab_size, self.vocab_size],
+                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(self.vocab_size)))
+
+        # batch_size x vocab_size, vocab_size x vocab_size --> batch_size x vocab_size
+        n_activation = tf.matmul(inputs, n_gating_weights)
+        n_activation = -1 * n_activation
+        n_activation = tf.nn.relu6(n_activation)
+        n_gate = inputs + (-1 * n_activation)
+
+        # batch_size x vocab_size, vocab_size x vocab_size --> batch_size x vocab_size
+        p_activation = tf.matmul(n_gate, p_gating_weights)
+        p_activation = tf.nn.relu6(p_activation)
+        p_gate = n_gate + p_activation
+        output = tf.nn.softmax(p_gate)
+
+        return output
+
+
+class PGateModule(modules.BaseModule):
+    def __init__(self, vocab_size, is_training, scope_id=None):
+        """ Initialize class PGateModule.
+        :param vocab_size: int
+            Size of the classes.
+        :param is_training: bool
+            True iff the model is being trained.
+        :param scope_id: Object
+        """
+        self.vocab_size = vocab_size
+        self.scope_id = scope_id
+        self.is_training = is_training
+
+    def forward(self, inputs, **unused_params):
+        """ PN Gate for correlation learning.
+        vocabularies -> P gate -> output
+        :param inputs: batch_size x vocab_size
+        :return: batch_size x vocab_size
+        """
+        p_gating_weights = \
+            tf.get_variable("p_p_gate",
+                            [self.vocab_size, self.vocab_size],
+                            initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(self.vocab_size)))
+
+        # batch_size x vocab_size, vocab_size x vocab_size --> batch_size x vocab_size
+        p_activation = tf.matmul(inputs, p_gating_weights)
+        p_activation = tf.nn.relu6(p_activation)
+        p_gate = inputs + p_activation
+        output = tf.nn.softmax(p_gate)
+
+        return output
+
+
+class CorNNGateModule(modules.BaseModule):
+    def __init__(self, vocab_size, is_training, batch_norm=True, scope_id=None):
+        """ Initialize a class CorNNGateModule.
+        :param vocab_size: int
+            Size of the classes.
+        :param is_training: bool
+        :param batch_norm: bool
+        :param scope_id: int
+        """
+        self.vocab_size = vocab_size
+        self.is_training = is_training
+        self.batch_norm = batch_norm
+        self.scope_id = scope_id
+
+    def forward(self, inputs, **unused_params):
+        """ Forward function of CorNNGateModule.
+        :param inputs: batch_size x vocab_size
+        :return: batch_size x vocab_size
+        """
+        fc1_out = slim.fully_connected(
+            inputs=inputs,
+            num_outputs=self.vocab_size,
+            activation_fn=nn.relu,
+            scope="vocab_gate1_v1{}".format("" if self.scope_id is None else str(self.scope_id))
+        )
+
+        fc2_out = slim.fully_connected(
+            inputs=fc1_out,
+            num_outputs=self.vocab_size,
+            activation_fn=nn.relu,
+            scope="vocab_gate2_v1{}".format("" if self.scope_id is None else str(self.scope_id))
+        )
+
+        fc3_out = slim.fully_connected(
+            inputs=fc2_out,
+            num_outputs=self.vocab_size,
+            activation_fn=nn.sigmoid,
+            scope="vocab_gate3_v1{}".format("" if self.scope_id is None else str(self.scope_id))
+        )
+
+        return fc3_out
+
 
 ###############################################################################
 # Attention / Context Gate for vocabularies ###################################
@@ -81,14 +207,15 @@ class ContextGateV1(modules.BaseModule):
     def __init__(self, vocab_size, is_training, batch_norm=True, scope_id=None):
         """ Initialize a class ContextGateV1. The idea and implementation is adopted from WILLOW.
         :param vocab_size: int
-            e.g. ~3600 for YouTube V2 dataset.
+            Size of the classes.
+        :param is_training: bool
         :param batch_norm: bool
         :param scope_id: int
         """
         self.vocab_size = vocab_size
+        self.is_training = is_training
         self.batch_norm = batch_norm
         self.scope_id = scope_id
-        self.is_training = is_training
 
     def forward(self, inputs, **unused_params):
         """ Forward function of ContextGateV1
