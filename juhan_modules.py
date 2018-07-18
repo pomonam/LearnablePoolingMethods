50cafb9bb169b30651fa4274233463e847892bbf
pomonam <pomonam15@gmail.com>
2018-07-18 10:43:19 -0400

    Added Juhan modules for temporary

---
+++ juhan_modules.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+import modules
+
+
+class JuhanTransformerEncoder:
+    def __init__(self, num_heads, num_units, max_frames, block_id):
+        """
+        :param num_heads: Number of self-attention modules
+        :param num_units: last dimension of Q, K, V
+        """
+        self.num_heads = num_heads
+        self.num_units = num_units
+        self.max_frames = max_frames
+        self.block_id = block_id
+
+    def scaled_dot_product(self, queries, keys, dropout_rate, is_training, head_id):
+        """
+        :param Q: batch_size x max_frames x num_units
+        :param K: batch_size x max_frames x num_units
+        :param V: batch_size x max_frames x num_units
+        :return:
+        """
+        with tf.variable_scope("multi_head_head{}".format(str(head_id))):
+            # Linear projections
+            q = tf.layers.dense(queries, self.num_units, activation=None)
+            k = tf.layers.dense(keys, self.num_units, activation=None)
+            v = tf.layers.dense(keys, self.num_units, activation=tf.nn.relu)
+            # -> batch_size x num_frames x num_units
+
+            outputs = tf.matmul(q, tf.transpose(k, [0, 2, 1]))
+            # -> batch_size x num_frames x num_frames
+            outputs = tf.divide(outputs, (k.get_shape().as_list()[-1] ** 0.5))
+            outputs = tf.nn.softmax(outputs)
+            outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+            outputs = tf.matmul(outputs, v)
+
+            return outputs
+
+    def forward(self, queries, keys, dropout_rate, is_training):
+        with tf.variable_scope("multi_head_block{}".format(str(self.block_id))):
+            result = self.scaled_dot_product(queries, keys, dropout_rate, is_training, head_id=0)
+            for i in range(1, self.num_heads):
+                result = tf.identity(result)
+                output = self.scaled_dot_product(queries, keys, dropout_rate, is_training, head_id=i)
+                result = tf.concat([result, output], 2)
+            # result: -> batch_size x max_frames x (num_units * num_heads)
+
+            # Fully connected layer.
+            reshaped_result = tf.reshape(result, [-1, self.num_units * self.num_heads])
+            reshaped_result = tf.layers.dense(reshaped_result, self.num_units, activation=tf.nn.relu)
+
+            activation = reshaped_result + queries
+            activation = tf.contrib.layers.layer_norm(activation)
+
+            dense1 = tf.layers.dense(activation, self.num_units, activation=tf.nn.relu)
+            dense2 = tf.layers.dense(dense1, self.num_units, activation=tf.nn.relu)
+
+            final_activation = activation + dense2
+            final_activation = tf.contrib.layers.layer_norm(final_activation)
+
+            return final_activation
+
+
+class JuhanTransformerNetVladDecoder:
+    def __init__(self, num_heads, num_units, max_frames, block_id):
+        """
+        :param num_heads: Number of self-attention modules
+        :param num_units: last dimension of Q, K, V
+        """
+        self.num_heads = num_heads
+        self.num_units = num_units
+        self.max_frames = max_frames
+        self.block_id = block_id
+
+    def scaled_dot_product(self, queries, keys, dropout_rate, is_training, head_id):
+        """
+        :param Q: batch_size x max_frames x num_units
+        :param K: batch_size x max_frames x num_units
+        :param V: batch_size x max_frames x num_units
+        :return:
+        """
+        with tf.variable_scope("multi_head_head{}".format(str(head_id))):
+            # Linear projections
+            q = tf.layers.dense(queries, self.num_units, activation=tf.nn.relu)
+            k = tf.layers.dense(keys, self.num_units, activation=tf.nn.relu)
+            v = tf.layers.dense(keys, self.num_units, activation=tf.nn.relu)
+            # -> batch_size x num_frames x num_units
+
+            outputs = tf.matmul(q, tf.transpose(k, [0, 2, 1]))
+            # -> batch_size x num_frames x num_frames
+            outputs = tf.divide(outputs, (k.get_shape().as_list()[-1] ** 0.5))
+            outputs = tf.nn.softmax(outputs)
+            outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+            outputs = tf.matmul(outputs, v)
+
+            return outputs
+
+    def forward(self, queries, keys, dropout_rate, is_training):
+        with tf.variable_scope("multi_head_block{}".format(str(self.block_id))):
+            result = self.scaled_dot_product(queries, keys, dropout_rate, is_training, head_id=0)
+            for i in range(1, self.num_heads):
+                result = tf.identity(result)
+                output = self.scaled_dot_product(queries, keys, dropout_rate, is_training, head_id=i)
+                result = tf.concat([result, output], 2)
+            # result: -> batch_size x max_frames x (num_units * num_heads)
+
+            # Fully connected layer.
+            reshaped_result = tf.reshape(result, [-1, self.num_units * self.num_heads])
+            reshaped_result = tf.layers.dense(reshaped_result, self.num_units, activation=tf.nn.relu)
+
+            activation = reshaped_result + queries
+            activation = tf.contrib.layers.layer_norm(activation)
+
+            dense1 = tf.layers.dense(activation, self.num_units, activation=tf.nn.relu)
+            dense2 = tf.layers.dense(dense1, self.num_units, activation=tf.nn.relu)
+
+            final_activation = activation + dense2
+            final_activation = tf.contrib.layers.layer_norm(final_activation)
+
+            return final_activation
