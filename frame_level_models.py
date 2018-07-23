406dbe06f039ac0e714c2693e318528a7dcae95a
pomonam <pomonam15@gmail.com>
2018-07-23 02:28:38 -0400

    ensure same shpae

--- frame_level_models.py
+++ frame_level_models.py
@@ -3396,7 +3396,7 @@ class NetVLADModelLF(models.BaseModel):
         audio_NetVLAD = fish_modules.LuckyFishModule(128, max_frames, 64, add_batch_norm, True, is_training)
 
         if add_batch_norm:  # and not lightvlad:
-            reshaped_input = slim.batch_norm(   
+            reshaped_input = slim.batch_norm(
                 reshaped_input,
                 center=True,
                 scale=True,
@@ -3405,9 +3405,11 @@ class NetVLADModelLF(models.BaseModel):
 
         with tf.variable_scope("video_VLAD"):
             vlad_video = video_NetVLAD.forward(reshaped_input[:, 0:1024])
+            vlad_video = tf.reshape(vlad_video, [-1, 256 * 1024])
 
         with tf.variable_scope("audio_VLAD"):
             vlad_audio = audio_NetVLAD.forward(reshaped_input[:, 1024:])
+            vlad_audio = tf.reshape(vlad_audio, [-1, 64 * 128])
 
         vlad = tf.concat([vlad_video, vlad_audio], 1)
 
