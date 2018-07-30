165c38c77d9f7187c472356e2794915bbf033665
pomonam <pomonam15@gmail.com>
2018-07-30 03:28:37 -0400

    Summon back bottleneck layer

--- frame_level_models.py
+++ frame_level_models.py
@@ -119,7 +119,10 @@ class CrazyFishV6(models.BaseModel):
                 audio_cluster_activation = audio_cluster.forward(audio_features)
 
         concat_activation = tf.concat([video_cluster_activation, audio_cluster_activation], 1)
-        activation0 = tf.layers.dense(concat_activation, vocab_size, use_bias=False, activation=None,
+        bottleneck = tf.layers.dense(concat_activation, hidden_size, use_bias=False, activation=None)
+        bottleneck = tf.layers.batch_normalization(bottleneck, training=is_training)
+
+        activation0 = tf.layers.dense(bottleneck, vocab_size, use_bias=False, activation=None,
                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_rate))
         if is_training:
             activation0 = tf.nn.dropout(activation0, linear_dropout)
