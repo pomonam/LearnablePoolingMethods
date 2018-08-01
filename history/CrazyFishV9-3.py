969d5ddd23a01188c0355ca4d21a21f03be5c22f
pomonam <pomonam15@gmail.com>
2018-08-01 11:39:19 -0400

    Fixed critical bug

--- history/CrazyFishV9-3.py
+++ history/CrazyFishV9-3.py
@@ -31,7 +31,7 @@ FRAME_LEVEL = True
 FEATURES = "rgb,audio"
 
 # Batch size.
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 
 # Base LR.
 BASE_LEARNING_RATE = 0.0002
@@ -43,13 +43,13 @@ EXTRA = "--fish9_iteration=300 " \
         "--fish9_video_cluster_size=256 " \
         "--fish9_audio_cluster_size=32 " \
         "--fish9_shift_operation=True " \
-        "--fish9_filter_size=2 " \
+        "--fish9_filter_size=4 " \
         "--fish9_cluster_dropout=0.8 " \
         "--fish9_ff_dropout=0.9 " \
         "--fish9_linear_proj_dropout=0.9 " \
         "--fish9_l2_regularization_rate=1e-8 " \
         "--fish9_hidden_size=1 " \
-        "--moe_num_mixtures=4 " \
+        "--moe_num_mixtures=2 " \
         "--learning_rate_decay=0.8 " \
         "--num_epochs=4 " \
         "--moe_l2=1e-6 " \
